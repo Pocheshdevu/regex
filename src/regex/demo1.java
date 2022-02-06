ddb062b8acc37b96e776210efca089e248a15056
Pochesh devu <pocheshd@gmail.com>
2022-02-06 12:24:17 +0530

    first commit

---
+++ src/regex/demo1.java
@@ -0,0 +1,29 @@
+package regex;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+
+public class demo1 {
+	
+	
+
+		public static void main(String[] args) {
+			
+			String regex="[a-z]+";
+			String input="tat";
+			
+			Pattern pattern= Pattern.compile(regex);
+			
+			Matcher match= pattern.matcher(input);
+			
+			if(match.matches())
+			{
+				System.out.println("Pattern Matched");
+			}
+			else
+			{
+				System.out.println("invalid input");
+			}
+		}
+	}
+
